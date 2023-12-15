c38e17a95c29b01f5053a4c85f9318c0b722f7c5
11C0DE11 <153782615+11C0DE11@users.noreply.github.com>
2023-12-15 15:28:49 +0000

    Create solution_part1.cpp

---
+++ 2023/Day 1/solution_part1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+int main() {
+    std::ifstream file("input");
+    std::string line;
+
+    if (!file.is_open()) {
+        std::cerr << "error opening input file" << '\n';
+        return -1;
+    }
+
+    int left, right, sum = 0;
+
+    while (std::getline(file, line)) {
+           left  = 0;
+	         right = line.size()-1;
+
+	         while (!std::isdigit(line[left])) left++;
+           while (!std::isdigit(line[right])) right--;
+        
+   	       if (left == right) {
+	             if (std::isdigit(line[left]))
+ 		               sum += ((line[left] - '0') * 10 + (line[left] - '0'));
+               else
+ 		               sum += ((line[right] - '0') * 10 + (line[right] - '0'));
+	         }
+	         else {
+               sum += ((line[left] - '0') * 10) + (line[right] - '0');	   
+	         }    
+    } 
+
+    std::cout << "Final sum: " << sum << '\n';
+
+    return 0;
+}
